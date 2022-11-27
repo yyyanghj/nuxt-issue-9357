0bf5b1cabb13afe5c527679ef2e6770147fd8541
yyyanghj <yyyanghj@gmail.com>
2022-11-28 00:55:46 +0800

    init

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Hello World
+  </div>
+</template>
+
+<script>
+// import "@fontsource/lora";
+</script>
+
+
+<style>
+@import "@fontsource/lora";
+
+body {
+  font-family: "Lora";
+  font-size: 100px;
+}
+</style>
